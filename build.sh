bd5fd8f2a6e054eb49903af10eba45386b28befd
Richard Jones <richard@cottagelabs.com>
2017-11-15 16:42:30 +0000

    add progress bars to build script and release

--- build.sh
+++ build.sh
@@ -44,7 +44,9 @@ cp $EDGES/src/edges.js $SRC
 cp $EDGES/src/edges.csv.js $SRC
 cp $EDGES/src/components/charts.js $SRC
 cp $EDGES/src/components/tables.js $SRC
+cp $EDGES/src/components/loading.js $SRC
 cp $EDGES/src/renderers/nvd3.edges.js $SRC
+cp $EDGES/src/renderers/loading-bar.edges.js $SRC
 cp src/js/scoss_edges.js $SRC
 
 # compile all the javascript down to minified versions
@@ -56,9 +58,12 @@ cat $BSRC/edges.jquery.js <(echo) \
     $BSRC/edges.csv.js <(echo) \
     $BSRC/charts.js <(echo) \
     $BSRC/tables.js <(echo) \
+    $BSRC/loading.js <(echo) \
+    $BSRC/loading-bar.edges.js <(echo) \
     $BSRC/nvd3.edges.js <(echo) \
     $BSRC/scoss_edges.js <(echo) \
     > $MSRC
+
 
 #######################################################
 ## JS Dependencies handling
@@ -68,12 +73,14 @@ cp $EDGES/vendor/jquery-1.11.1/jquery-1.11.1.min.js $DEP
 cp $EDGES/vendor/d3-3.5.17/d3.min.js $DEP
 cp $EDGES/vendor/nvd3-1.8.1/nv.d3.js $DEP
 cp $EDGES/vendor/PapaParse-4.1.2/papaparse.min.js $DEP
+cp $EDGES/vendor/loading-bar/loading-bar.min.js $DEP
 
 # combine all the dependencies into a single file in the right order
 cat $DEP/jquery-1.11.1.min.js <(echo) \
     $DEP/d3.min.js <(echo) \
     $DEP/nv.d3.js <(echo) \
     $DEP/papaparse.min.js <(echo) \
+    $DEP/loading-bar.min.js <(echo) \
     > $MDEP
 
 
@@ -82,14 +89,16 @@ cat $DEP/jquery-1.11.1.min.js <(echo) \
 
 # copy the css into the build directory
 cp src/css/scoss_edges.css $CSS
+cp $EDGES/css/loading-bar.edges.css $CSS
 
 # minify each css file individually
 nodejs $R -o cssIn=$CSS/scoss_edges.css out=$BCSS/scoss_edges.css baseUrl=.
+nodejs $R -o cssIn=$CSS/loading-bar.edges.css out=$BCSS/loading-bar.edges.css baseUrl=.
 
 # cat all the CSS into a single minified file
 cat $BCSS/scoss_edges.css <(echo) \
+    $BCSS/loading-bar.edges.css <(echo) \
     > $MCSS
-
 
 ########################################################
 ## CSS Dependencies handling
@@ -97,12 +106,13 @@ cat $BCSS/scoss_edges.css <(echo) \
 # copy all the css into the build directory
 cp $EDGES/vendor/bootstrap-3.3.1/css/bootstrap.min.css $CSS
 cp $EDGES/vendor/nvd3-1.8.1/nv.d3.css $CSS
+cp $EDGES/vendor/loading-bar/loading-bar.css $CSS
 
 # cat all the dependency CSS into a single file
 cat $CSS/bootstrap.min.css <(echo) \
     $CSS/nv.d3.css <(echo) \
+    $CSS/loading-bar.css <(echo) \
     > $CDEP
-
 
 #######################################################
 ## Record the Build time and cleanup
