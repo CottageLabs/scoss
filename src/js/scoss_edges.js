6901ee9078763f11c9ca18fb0fc5a5801e424554
Richard Jones <richard@cottagelabs.com>
2017-11-20 14:36:06 +0000

    tweaks to funds by country page section

--- src/js/scoss_edges.js
+++ src/js/scoss_edges.js
@@ -192,6 +192,16 @@ var scoss = {
         }
     },
 
+    euroFormatter : edges.numFormat({
+        prefix: "&euro;",
+        thousandsSeparator: ","
+    }),
+
+    currencyFormatter : edges.numFormat({
+        prefix: "€",
+        thousandsSeparator: ","
+    }),
+
     makeServiceProviderPage : function(params) {
         scoss.activeEdges[params.selector] = edges.newEdge({
             selector: params.selector,
@@ -220,10 +230,11 @@ var scoss = {
                         preset: "circle",
                         title: "<h3>Committed</h3>",
                         showXofY: true,
-                        xyNumFormat: edges.numFormat({
-                            prefix: "&euro;",
-                            thousandsSeparator: ","
-                        })
+                        //xyNumFormat: edges.numFormat({
+                        //    prefix: "&euro;",
+                        //    thousandsSeparator: ","
+                        //})
+                        xyNumFormat: scoss.euroFormatter
                     })
                 }),
                 edges.loading.newLoadingBar({
@@ -233,10 +244,7 @@ var scoss = {
                         preset: "circle",
                         title: "<h3>Paid</h3>",
                         showXofY: true,
-                        xyNumFormat: edges.numFormat({
-                            prefix: "&euro;",
-                            thousandsSeparator: ","
-                        })
+                        xyNumFormat: scoss.euroFormatter
                     })
                 }),
                 edges.loading.newLoadingBar({
@@ -246,20 +254,21 @@ var scoss = {
                         preset: "circle",
                         title: "<h3>Needed</h3>",
                         showXofY: true,
-                        xyNumFormat: edges.numFormat({
-                            prefix: "&euro;",
-                            thousandsSeparator: ","
-                        })
+                        xyNumFormat: scoss.euroFormatter
                     })
                 }),
                 edges.newHorizontalMultibar({
                     id: "by_country",
                     dataFunction: scoss.countryDF,
                     renderer : edges.nvd3.newHorizontalMultibarRenderer({
+                        legend: false,
                         dynamicHeight: true,
                         barHeight: 15,
                         reserveAbove: 50,
-                        reserveBelow: 50
+                        reserveBelow: 50,
+                        yTickFormat: scoss.currencyFormatter,
+                        valueFormat: scoss.currencyFormatter,
+                        yAxisLabel: "Total Funding (EUR)"
                     })
                 }),
                 edges.newPieChart({
@@ -304,6 +313,7 @@ var scoss = {
             this.edge = edge;
 
             var containerClass = edges.css_classes(this.namespace, "container");
+            var sectionClass = edges.css_classes(this.namespace, "section");
             var progressCommittedClass = edges.css_classes(this.namespace, "progress_committed");
             var progressPaidClass = edges.css_classes(this.namespace, "progress_paid");
             var progressNeededClass = edges.css_classes(this.namespace, "progress_needed");
@@ -313,18 +323,29 @@ var scoss = {
             var allDonorsClass = edges.css_classes(this.namespace, "all-donors");
 
             var frag = '<div class="' + containerClass + '">\
-                    <div class="row"><div class="col-md-12"><h2>Funding Progress</h2></div></div>\
-                    <div class="row">\
-                        <div class="col-md-2 col-md-offset-2"><div id="progress_committed" class="' + progressCommittedClass + '"></div></div>\
-                        <div class="col-md-2 col-md-offset-1"><div id="progress_paid" class="' + progressPaidClass + '"></div></div>\
-                        <div class="col-md-2 col-md-offset-1"><div id="progress_needed" class="' + progressNeededClass + '"></div></div>\
+                    <div class="' + sectionClass + '">\
+                        <div class="row"><div class="col-md-12"><h2>Funding Progress</h2></div></div>\
+                        <div class="row">\
+                            <div class="col-md-2 col-md-offset-2"><div id="progress_committed" class="' + progressCommittedClass + '"></div></div>\
+                            <div class="col-md-2 col-md-offset-1"><div id="progress_paid" class="' + progressPaidClass + '"></div></div>\
+                            <div class="col-md-2 col-md-offset-1"><div id="progress_needed" class="' + progressNeededClass + '"></div></div>\
+                        </div>\
+                    </div> \
+                    <div class="' + sectionClass + '">\
+                        <div class="row"><div class="col-md-12">\
+                            <h2>Funds by Country</h2>\
+                            <div id="by_country" class="' + countryClass + '"></div>\
+                        </div></div>\
                     </div>\
-                    <div class="row"><div class="col-md-12">\
-                        <div id="by_country" class="' + countryClass + '"></div>\
-                    </div></div>\
-                    <div class="row"><div class="col-md-12"><div id="by_continent" class="' + continentClass + '"></div></div></div>\
-                    <div class="row"><div class="col-md-12"><div id="top_donors" class="' + topDonorsClass + '"></div></div></div>\
-                    <div class="row"><div class="col-md-12"><div id="all_donors" class="' + allDonorsClass + '"></div></div></div>\
+                    <div class="' + sectionClass + '">\
+                        <div class="row"><div class="col-md-12"><div id="by_continent" class="' + continentClass + '"></div></div></div>\
+                    </div>\
+                    <div class="' + sectionClass + '">\
+                        <div class="row"><div class="col-md-12"><div id="top_donors" class="' + topDonorsClass + '"></div></div></div>\
+                    </div>\
+                    <div class="' + sectionClass + '">\
+                        <div class="row"><div class="col-md-12"><div id="all_donors" class="' + allDonorsClass + '"></div></div></div>\
+                    </div>\
                 </div></div>';
 
             edge.context.html(frag);
